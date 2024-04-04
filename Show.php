6fc7f50d7bcab8610938c33f8f43df516d808011
Aek <kobx2.2005@gmail.com>
2024-04-04 23:23:46 +0700

    hello

---
+++ Show.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" type="png" href="img/Logo.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Show</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body>
+    <header class="info">
+        <div class="container">
+            <div class="content">
+                <div class="profile">
+                </div>
+                <div class="text">
+                    <a href="https://www.facebook.com/aekaphon.xayyabed.37?mibextid=ZbWKwL">
+                        <h2>ສະກິດຕິດຕົວ</h2>
+                        <p>All abilities</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="info4">
+        <div class="container">
+            <div class="container-glr">
+                <div class="gelery">
+                    <img src="img/p1.png" alt="" width="153" height="153">
+                    <img src="img/p2.png" alt="" width="153" height="153">
+                </div>
+                <div class="gelery">
+                    <img src="img/p3.png" alt="" width="153" height="153">
+                    <img src="img/p4.png" alt="" width="153" height="153">
+                </div>
+
+                <div class="gelery">
+                    <img src="img/p5.png" alt="" width="153" height="153">
+                    <img src="img/p6.png" alt="" width="153" height="153">
+                </div>
+
+                <div class="gelery">
+                    <img src="img/p7.png" alt="" width="153" height="153">
+                    <img src="img/p8.png" alt="" width="153" height="153">
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section class="back">
+        <div class="container">
+            <div class="btn-back">
+                <div class="button">
+                    <a class="btn"
+                        href="index.php">
+                       ອັບເດດຕັ້ງລ່າສຸດ 4/4/2024 
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" type="png" href="img/Logo.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile AEK</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header class="info">
+        <div class="container">
+            <div class="content">
+                <div class="profile">
+                </div>
+                <div class="text">
+                    <a href="https://www.facebook.com/aekaphon.xayyabed.37?mibextid=ZbWKwL">
+                        <h2 >Mr: AEK.K</h2>
+                        <p style="font-family: Noto Sans Lao;">ສະກິວແລະຜົນງານ</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="info1">
+        <div class="container">
+            <div class="content2">
+                <div class="button">
+                    <a class="btn"
+                        href="https://drive.google.com/drive/folders/1wgNrRS1CrAob5CZ-uvhm5VSh8ykYn2rh?fbclid=IwAR0WwE-cjM02kkdB3W3zIcj2ZXpb38yQ-z7BRlf2jo5vI1ni8NulcGPyki8_aem_ATZo1xGJh4uxTloWVl2_D1y-W-aDopkCrvv4KGIZH4UiM3HrlyoqFw9GciT7koAGSe5y7yzQvghokuFEKuxhXUWG">
+                        <img src="img/Group 15.png" alt="" width="314" height="84">
+                    </a>
+                    <a class="btn" href="Show.php">
+                        <img src="img/Group 13.png" alt="" width="314" height="84">
+                    </a>
+                    <a class="btn" href="https://kobxplorer.gumroad.com/l/MobilePosterV2">
+                        <img src="img/Group 16.png" alt="" width="314" height="84">
+                    </a>
+                    <a class="btn" href="https://wa.me/qr/MHDDZBSMSY2EE1">
+                        <img src="img/Group 14.png" alt="" width="314" height="84">
+                    </a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <!-- <footer class="info3">
+        <div class="container">
+            <div class="content3">
+                <div class="profile3">
+                </div>
+                <div class="text3">
+                    <a href="https://web.facebook.com/BeginSportJersey">
+                        <h2>ຮ້ານ ບີຈີ ສປອຣ໌ດ</h2>
+                        <p>BG SPorts Jersey</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer> -->
+
+</body>
+
+</html>
